ec15ec9b8e791d8468d73e810f5fb41972f3f50b
Pavel Punegov <pavel.punegov@jetbrains.com>
2022-05-26 17:14:23 +0300

    Replace tokens with properties

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -23,9 +23,11 @@ repositories {
 
     maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
 
+    fun getSensitiveProperty(name: String) = project.findProperty(name) as? String ?: System.getenv(name)
+
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-atomicfu/maven").credentials {
-        username = "margarita.bobova"
-        password = "eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIxcm1UZ20wbEFKaEoiLCJhdWQiOiJjaXJjbGV0LXdlYi11aSIsIm9yZ0RvbWFpbiI6InB1YmxpYyIsIm5hbWUiOiJtYXJnYXJpdGEuYm9ib3ZhIiwiaXNzIjoiaHR0cHM6XC9cL3B1YmxpYy5qZXRicmFpbnMuc3BhY2UiLCJwZXJtX3Rva2VuIjoiSVBwZlkwQ3M1cjUiLCJwcmluY2lwYWxfdHlwZSI6IlVTRVIiLCJpYXQiOjE2NDk5MjM2NDF9.olTvoKz6KSX1rMCkid3vCSvwy-95rQTYL9gVlj7ueudTEVGqXaq1tJc37FDnKL6i6oc26XLVDK0y4G_B7ZKJGoMh77nckx-XMmRxB4Q3LZY1cXo_Mt4zD9lPxfFAfHW9RboJFgNlLWzg3OVQvMwDgHetYhnuGmlTtzCKfCW3Ke4"
+        username = getSensitiveProperty("spaceUsername")
+        password = getSensitiveProperty("spacePassword")
     }
     if (buildSnapshotTrain) {
         mavenLocal()
